7eba816bfdf90c7f7acbe800b075abbe40c12905
magnusarge <110186971+magnusarge@users.noreply.github.com>
2023-02-27 18:27:58 +0200

    New parameters
    To make script more dynamic.

--- Router.php
+++ Router.php
@@ -4,21 +4,23 @@ $routes = [
     ["route"=>"/contacts", "file"=>"/sample_pages/contacts.html"]
 ];
 
-$router = new Router($routes);
+$router = new Router(__DIR__, $_SERVER['PHP_SELF'], $routes);
 
 class Router
 {
+    private string $diskDir;
     private string $scriptRoot;
     private string $routeUrl;
     private bool $siteIsInSubfolder;
     private $exitAfterSuccess = true;
     private $exitAfterNotFound = true;
-    private $error404file = "/sample_pages/404.html";
+    private $error404file = "/pages/404.html";
 
-    public function __construct($routes=[])
+    public function __construct($diskDir, $phpself, $routes=[])
     {
 
-        $this->scriptRoot = dirname($_SERVER['PHP_SELF']);
+        $this->diskDir = $diskDir;
+        $this->scriptRoot = dirname($phpself);
         $this->siteIsInSubfolder = $this->scriptRoot != '/';
 
         $routeUrl = parse_url($_SERVER['REQUEST_URI']);
@@ -27,7 +29,7 @@ class Router
             $this->routeUrl = substr($this->routeUrl, strlen($this->scriptRoot));
         }
 
-        if ( !file_exists(__DIR__ . '/.htaccess') ) $this->createHtaccess();
+        if ( !file_exists($this->diskDir . '/.htaccess') ) $this->createHtaccess();
 
         $this->route($routes);
 
@@ -37,8 +39,8 @@ class Router
     {
         foreach ($routes as $route) {
             if ( isset($route['route']) && $this->compareRoutes($route['route'], $this->routeUrl) ) {
-                if ( isset($route['file']) && file_exists(__DIR__ . $route['file']) ) {
-                    include __DIR__ . $route['file'];
+                if ( isset($route['file']) && file_exists($this->diskDir . $route['file']) ) {
+                    include $this->diskDir . $route['file'];
                     if ( $this->exitAfterSuccess ) exit();
                     return;
                 }
@@ -70,8 +72,8 @@ class Router
         header(header: "HTTP/1.0 404 Not Found");
         if ( $this->exitAfterNotFound ) exit();
         
-        if ( file_exists(__DIR__ . $this->error404file) ) {
-            include __DIR__ . $this->error404file;
+        if ( file_exists($this->diskDir . $this->error404file) ) {
+            include $this->diskDir. $this->error404file;
         }
     }
 
@@ -84,6 +86,6 @@ class Router
             "RewriteCond %%{REQUEST_FILENAME} !-f\n".
             "RewriteRule ^(.*)$ index.php [QSA,L]",
             $this->scriptRoot);
-        file_put_contents(__DIR__ . '/.htaccess', $htaccess);
+        file_put_contents($this->diskDir . '/.htaccess', $htaccess);
     }
 }
